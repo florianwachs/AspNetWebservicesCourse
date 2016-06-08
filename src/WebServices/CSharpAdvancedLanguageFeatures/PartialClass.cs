9be36208f6481e68f73dc087f341c5a805a6dc90
fexdev <florian.wachs@live.de>
2016-06-08 15:35:31 +0200

    CSharpAdvancedLanguageFeatures

---
+++ src/WebServices/CSharpAdvancedLanguageFeatures/PartialClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvancedLanguageFeatures
+{
+    public static class PartialDemo
+    {
+        public static void Demo1()
+        {
+            var p = new PartialClass();
+            Console.WriteLine(p.LastChanged.HasValue);
+            p.GeneratedField = 10;
+            Console.WriteLine(p.LastChanged.HasValue);
+        }
+    }
+
+    public partial class PartialClass
+    {
+        public DateTime? LastChanged { get; private set; }
+
+        // partial Methode wird implementiert und
+        // damit vom Compiler auch berücksichtigt
+        partial void Changed(int? oldValue, int? newValue)
+        {
+            LastChanged = DateTime.Now;
+        }
+    }
+}
