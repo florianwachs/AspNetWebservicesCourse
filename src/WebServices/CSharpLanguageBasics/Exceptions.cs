4c1ea49957dd0c8e8a88a3515d5b00eddd1f6bf5
fexdev <florian.wachs@live.de>
2016-06-08 15:30:40 +0200

    CSharpLanguageBasics

---
+++ src/WebServices/CSharpLanguageBasics/Exceptions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLanguageBasics
+{
+    public static class Exceptions
+    {
+        public static void NotDoneYet()
+        {
+            throw new NotImplementedException("Hab´s doch gesagt!");
+        }
+
+        public static void Demo1()
+        {
+            try
+            {
+                NotDoneYet();
+            }
+            catch (InvalidOperationException)
+            {
+                // Fängt nur Exceptions von diesem Typ
+            }
+            catch (Exception ex)
+            {
+                // Fängt alle restlichen Exceptions
+
+                // wirft die Exception weiter den Stacktrace rauf
+                throw ex;
+            }
+            finally
+            {
+                // Wird auf "jeden" Fall ausgeführt
+                // Aufräumen, z.B. File-Handles schließen
+            }
+        }
+    }
+}
---
+++ src/WebServices/CSharpLanguageBasics/Properties.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLanguageBasics
+{
+    public class Properties
+    {
+        // Properties mit Backing-Field
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            private set
+            {
+                age = value;
+            }
+        }
+
+        // Readonly Properties mit Custom Logic
+        public string AgeMessage
+        {
+            get
+            {
+                var message = "";
+
+                if (age < 30)
+                    message = "Genieße das Leben";
+                else
+                {
+                    message = "Das Ende naht...";
+                }
+
+                return message;
+            }
+        }
+
+        // auto-property
+        public string FirstName { get; private set; }
+
+        // C# 6 Feature
+        // public DateTime TimeStamp { get; } = DateTime.UtcNow;
+    }
+}
