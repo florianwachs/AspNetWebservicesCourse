600360c16f26d9d4b09241cffca0c587454b1485
fexdev <florian.wachs@live.de>
2016-06-09 08:47:10 +0200

    AdoNET.EFCodeFirst

---
+++ src/WebServices/AdoNET.EFCodeFirst/Configurations/StudentConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using AdoNET.EFCodeFirst.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNET.EFCodeFirst.Configurations
+{
+    public class StudentConfiguration : EntityTypeConfiguration<Student>
+    {
+        public StudentConfiguration()
+        {
+            Property(s => s.Motivation).HasPrecision(18, 2);
+        }
+    }
+}
