8ef9feab5f4eaaa5fe2602a0e71a7337f2f9ab97
fexdev <florian.wachs@live.de>
2016-06-09 10:42:23 +0200

    Exercise WCF and SOAP

---
+++ src/Exercises/WCF.Soap/WCF.Soap.QuizService/IQuizService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WCF.Soap.QuizService.Domain;
+
+namespace WCF.Soap.QuizService
+{
+    public interface IQuizService
+    {
+        QuizFrage HoleFrage();
+        QuizResultat BeantworteFrage(int frageID, int antwortIndex);
+    }
+}
