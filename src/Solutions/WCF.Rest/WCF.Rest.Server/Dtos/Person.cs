da69d65566ab0fcb1705cf8455b5a341778abd7f
fexdev <florian.wachs@live.de>
2016-06-20 17:02:01 +0200

    Solution WCF.Rest

---
+++ src/Solutions/WCF.Rest/WCF.Rest.Server/Dtos/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WCF.Rest.Server.Dtos
+{
+    [DataContract(Namespace = "http://www.fh-rosenheim.de/schema/dtos")]
+    public class Person
+    {
+        [DataMember]
+        public string Age { get; set; }
+        [DataMember]
+        public string Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
