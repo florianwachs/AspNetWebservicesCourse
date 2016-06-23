659aaf42c93763fca8a7863ba0782bf6e44dbc9e
fexdev <florian.wachs@live.de>
2016-06-23 20:20:19 +0200

    Client für Übung

---
+++ src/Exercises/WebAPI.EF/WebAPI.Server/DataAccess/IPersonRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Server.Models;
+
+namespace WebAPI.Server.DataAccess
+{
+    public interface IPersonRepository
+    {
+        IEnumerable<Person> GetAll();
+        Person GetById(string id);
+        Person Add(Person p);
+        Person Update(string id, Person p);
+        bool Delete(string id);
+    }
+}
---
+++ src/Exercises/WebAPI.EF/WebAPI.Server/DataAccess/InMemoryPersonService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPI.Server.Models;
+
+namespace WebAPI.Server.DataAccess
+{
+    public class InMemoryPersonService : IPersonRepository
+    {
+        private static int id = 4;
+        private static readonly Dictionary<string, Person> personen = new Dictionary<string, Person>{
+            {"1", new Person{Id="1", Name="Jason Bourne", Age="30"}},
+            {"2", new Person{Id="2", Name="Captain America", Age="80"}},
+            {"3", new Person{Id="3", Name="Tony Stark", Age="40"}},
+        };
+
+        public Person Add(Person p)
+        {
+            p.Id = GetUniqueId();
+            personen.Add(p.Id, p);
+            return p;
+        }
+
+        public bool Delete(string id)
+        {
+            return personen.Remove(id);
+        }
+
+        public IEnumerable<Person> GetAll()
+        {
+            return personen.Values;
+        }
+
+        public Person GetById(string id)
+        {
+            Person p;
+            return personen.TryGetValue(id, out p) ? p : null;
+        }
+
+        public Person Update(string id, Person p)
+        {
+            if (!personen.ContainsKey(id))
+            {
+                return null;
+            }
+
+            p.Id = id;
+            personen[id] = p;
+            return p;
+        }
+        private string GetUniqueId()
+        {
+            return (id++).ToString();
+        }
+    }
+}
