d1d93335f79306525fb40fd3242f7dc83e65e756
fexdev <florian.wachs@live.de>
2016-06-29 09:45:59 +0200

    AspNetMVC5

---
+++ src/WebServices/AspNetMVC5/Models/BookModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMVC5.Models
+{
+    public class BookModel
+    {
+        [Required]
+        public string Isbn { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/WebServices/AspNetMVC5/Models/QueryRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMVC5.Models
+{
+    public class QueryRequest
+    {
+        public int Page { get; set; }
+        public int CountPerPage { get; set; }
+    }
+}
