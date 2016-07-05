6e4b185ce84f844d581779afe96b2a6e19a080c0
fexdev <florian.wachs@live.de>
2016-07-05 16:31:49 +0200

    WebAPI.AutoMapper

---
+++ src/WebServices/WebAPI.Automapper/DataAccess/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Automapper.Models;
+
+namespace WebAPI.Automapper.DataAccess
+{
+    public interface IBookRepository
+    {
+        IEnumerable<Book> GetAll();
+        Book GetById(int id);
+        Book Add(Book p);
+        Book Update(int id, Book p);
+        bool Delete(int id);
+    }
+}
---
+++ src/WebServices/WebAPI.Automapper/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPI.Automapper.WebApiApplication" Language="C#" %>
