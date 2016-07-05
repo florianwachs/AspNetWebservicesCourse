d43de3c206d58548c79f57a1d9d385514241b5bb
fexdev <florian.wachs@live.de>
2016-07-05 09:18:27 +0200

    WCF.Security

---
+++ src/WebServices/WCF.Security/ICustomSecurityService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF.Security
+{
+    [ServiceContract]
+    public interface ICustomSecurityService
+    {
+        [OperationContract]
+        string DoWork();
+    }
+}
---
+++ src/WebServices/WCF.Security/TransportSecurityService.svc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF.Security
+{
+    public class TransportSecurityService : ITransportSecurityService
+    {
+        public string DoWork()
+        {
+            return "Hello World";
+        }
+    }
+}
