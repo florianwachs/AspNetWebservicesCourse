db1eababdced3d968f31b97dd3cca7b16e2b0f4a
fexdev <florian.wachs@live.de>
2016-07-06 10:22:03 +0200

    OData Init

---
+++ src/WebServices/WebAPI.OData/App_Start/WebApiConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+using WebAPI.OData.Models;
+
+namespace WebAPI.OData
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            //config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+
+            builder.EntitySet<Book>("Books");
+            builder.EntitySet<Author>("Authors");
+
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: "odata",
+                model: builder.GetEdmModel());
+        }
+    }
+}
---
+++ src/WebServices/WebAPI.OData/Controllers/AuthorsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.OData;
+using WebAPI.OData.DataAccess;
+using WebAPI.OData.Models;
+
+namespace WebAPI.OData.Controllers
+{
+    public class AuthorsController : ODataController
+    {
+        BookDbContext db = new BookDbContext();
+
+        [EnableQuery]
+        public IQueryable<Author> Get()
+        {
+            return db.Authors;
+        }
+
+        [EnableQuery]
+        public SingleResult<Author> Get([FromODataUri] int key)
+        {
+            IQueryable<Author> result = db.Authors.Where(p => p.Id == key);
+            return SingleResult.Create(result);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
