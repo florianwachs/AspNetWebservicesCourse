be87c40fd521057209e0d7876c6a7066ce07c6c1
fexdev <florian.wachs@live.de>
2017-05-10 19:51:02 +0200

    Simple Controller

---
+++ src/AspNetCore/AspNetCore.SimpleControllerSample/Controllers/SimpleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.SimpleControllerSample.Controllers
+{
+    [Route("api/[controller]")]
+    public class SimpleController : Controller
+    {
+        [HttpGet]
+        public string GetGreeting()
+        {
+            return "Hello World";
+        }
+    }
+}
