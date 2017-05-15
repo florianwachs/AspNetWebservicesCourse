db23e3afc85fe6793d129f6b0601dae1a0c46dc8
fexdev <florian.wachs@live.de>
2017-05-15 21:53:04 +0200

    serilog

---
+++ src/AspNetCore/AspNetCore.Logging.Serilog/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace AspNetCore.Logging.Serilog
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            Log.Logger = new LoggerConfiguration()
+                         .Enrich.FromLogContext()
+                         .WriteTo.LiterateConsole()
+                         .CreateLogger();
+
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime applicationLifetime)
+        {
+            loggerFactory.AddSerilog();
+            loggerFactory.AddDebug();
+            applicationLifetime.ApplicationStopped.Register(Log.CloseAndFlush);
+
+            app.UseMvc();
+        }
+    }
+}
