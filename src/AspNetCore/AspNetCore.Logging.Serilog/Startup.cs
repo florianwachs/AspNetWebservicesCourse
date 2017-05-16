108f8cfe76c9e2f38694b6fb463833a792eb0448
fexdev <florian.wachs@live.de>
2017-05-16 19:47:23 +0200

    better comments

--- src/AspNetCore/AspNetCore.Logging.Serilog/Startup.cs
+++ src/AspNetCore/AspNetCore.Logging.Serilog/Startup.cs
@@ -36,18 +36,18 @@ namespace AspNetCore.Logging.Serilog
 
         public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime applicationLifetime)
         {
             loggerFactory.AddSerilog();
             loggerFactory.AddDebug();
+
+            // Sicherstellen das beim Beenden der Anwendung alle Logs an die
+            // konfigurierten Sinks weitergegeben wurden
             applicationLifetime.ApplicationStopped.Register(Log.CloseAndFlush);
 
             app.UseMvc();
