d0693480bea3a8502ec946c2e116469bfb8728ab
Florian Wachs <florian.wachs@live.de>
2017-05-18 23:02:02 +0200

    EF

---
+++ src/AspNetCore/AspNetCore.EFRepository/Repositories/BookRepositoryEF.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore.EFRepository.Models;
+
+namespace AspNetCore.EFRepository.Repositories
+{
+    public class BookRepositoryEF : IBookRepository
+    {
+        private BookDbContext BookDbContext { get; set; }
+        public BookRepositoryEF(BookDbContext bookDbContext)
+        {
+            BookDbContext = bookDbContext;
+
+        }
+
+        public async Task<Book> Add(Book book)
+        {
+            var result = await BookDbContext.Books.AddAsync(book);
+            await BookDbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var book = await GetById(id);
+            if (book == null)
+            {
+                return false;
+            }
+
+            BookDbContext.Books.Remove(book);
+            await BookDbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+            return BookDbContext.Books;
+        }
+
+        public Task<Book> GetById(int id)
+        {
+            return BookDbContext.Books.FindAsync(id);
+        }
+
+        public async Task<Book> Update(int id, Book book)
+        {
+            book.Id = id;
+            var result = BookDbContext.Books.Update(book);
+            await BookDbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+    }
+}
---
+++ src/AspNetCore/AspNetCore.EFRepository/Repositories/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using AspNetCore.EFRepository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.EFRepository.Repositories
+{
+    public interface IBookRepository
+    {
+        IEnumerable<Book> GetAll();
+        Task<Book> GetById(int id);
+        Task<Book> Add(Book p);
+        Task<Book> Update(int id, Book p);
+        Task<bool> Delete(int id);
+    }
+}
