243fff1590ae6dc72c71116a7ca89596fa13eae0
Florian Wachs <florian.wachs@live.de>
2017-05-27 21:16:37 +0200

    Unified Extensions

--- src/AspNetCore/AspNetCore.BooksClient/HttpClientExtensions.cs
+++ src/AspNetCore/AspNetCore.BooksClient/HttpClientExtensions.cs
@@ -26,5 +26,14 @@ namespace AspNetCore.BooksClient
 
             return client.SendAsync(request);
         }
+
+        public static async Task<T> ReadAsAsync<T>(this HttpContent content) where T : class
+        {
+            var rawJson = await content.ReadAsStringAsync();
+            if (string.IsNullOrWhiteSpace(rawJson))
+                return null;
+
+            return JsonConvert.DeserializeObject<T>(rawJson);
+        }
     }
 }
