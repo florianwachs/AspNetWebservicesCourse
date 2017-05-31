ff3fb187f6c04a02a5c3f4d5b7a28dd637685299
Florian Wachs <florian.wachs@live.de>
2017-05-31 21:19:49 +0200

    refactor auth config

--- src/AspNetCore/AspNetCore.Security.OpenIddict/Startup.cs
+++ src/AspNetCore/AspNetCore.Security.OpenIddict/Startup.cs
@@ -1,20 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using AspNetCore.Security.OpenIddict.Models;
-using Microsoft.EntityFrameworkCore;
-using AspNet.Security.OpenIdConnect.Primitives;
-using Microsoft.AspNetCore.Identity;
-using System.Security.Claims;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using AspNetCore.Security.OpenIddict.Infrastructure;
 
 namespace AspNetCore.Security.OpenIddict
 {
@@ -36,94 +24,8 @@ namespace AspNetCore.Security.OpenIddict
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            ConfigureAuth(services);
-        }
 
-        private void ConfigureAuth(IServiceCollection services)
-        {
-            // Die Services für Identity konfigurieren            
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<IdentityContext>()
-                .AddDefaultTokenProviders();
-
-            // Entity Framework bezogene Configuration für OpenIddict
-            services.AddDbContext<IdentityContext>(options =>
-            {
-                options.UseSqlServer(Configuration["ConnectionStrings:IdentityContext"]);
-
-                // Fügt die von OpenIddict benötigten Entitäten und Modelklassen
-                // dem Entity Framework Context hinzu
-                options.UseOpenIddict();
-            });
-
-
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy(AppPolicies.CanAccessCustomerSaleHistory, policy => policy.RequireClaim(AppClaimTypes.ManagerId));
-                options.AddPolicy(AppPolicies.CanReadCustomerAge, policy => policy.RequireClaim(AppClaimTypes.Department, Departments.CustomerManagement));
-                options.AddPolicy(AppPolicies.CanDeleteCustomer, policy => policy.RequireClaim(AppClaimTypes.Department, Departments.CustomerManagement));
-                options.AddPolicy(AppPolicies.CanUpdateCustomer, policy => policy.RequireClaim(AppClaimTypes.Department, Departments.CustomerManagement, Departments.CustomerSupport));
-                options.AddPolicy(AppPolicies.CanCreateCustomer, policy => policy.RequireClaim(AppClaimTypes.Department, Departments.CustomerManagement, Departments.CustomerSupport));
-
-                options.AddPolicy("TopSecret", policy =>
-                {
-                    policy.RequireClaim(AppClaimTypes.AccessLevel, "A1")
-                    .RequireRole("Executive Manager")
-                    .RequireAssertion(authContext =>
-                        authContext.User.HasClaim(claim =>
-                            claim.Type == AppClaimTypes.Age && int.TryParse(claim.Value, out int age) && age > 18));
-                });
-            });
-            
-            services.Configure<IdentityOptions>(options =>
-            {
-                // Die JWT-Token-Claims statt den WS-Federation verwenden (die nimmt Identity by default)
-                // Identity verwendet standardmäßig nicht die JWT-Tokens, kann aber hiermit umkonfiguriert werden
-                options.ClaimsIdentity.UserNameClaimType = OpenIdConnectConstants.Claims.Name;
-                options.ClaimsIdentity.UserIdClaimType = OpenIdConnectConstants.Claims.Subject;
-                options.ClaimsIdentity.RoleClaimType = OpenIdConnectConstants.Claims.Role;
-
-                // Passwort-Regeln können sehr granular festgelegt werden
-                options.Password.RequireDigit = false;
-                options.Password.RequiredLength = 1;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequireLowercase = false;
-
-                // Auch Regeln für den Usernamen können festgelegt werden
-                //options.User.AllowedUserNameCharacters = "";
-            });
-
-            services.AddOpenIddict(options =>
-            {
-                // Register the Entity Framework stores.
-                options.AddEntityFrameworkCoreStores<IdentityContext>();
-
-                // ModelBinder von OpenIddict registerieren.
-                // Damit können Request-Parameter wieOpenIdConnectRequest oder OpenIdConnectResponse verwendet werden.
-                // Diese werden meist in einem AuthorizationController verwendet um einen Autorisierungsrequest verarbeiten zu können.
-                options.AddMvcBinders();
-
-                // Endpunkte aktivieren
-                // Hier wird registriert unter welchem Endpunkt ein Token angefragt werden kann.
-                options.EnableTokenEndpoint("/connect/token")
-                .EnableUserinfoEndpoint("/api/userinfo");
-
-                // Der OAuth-Standard definiert verschiedene Möglichkeiten sich zu authentifizieren
-                // und einen Token zu erhalten. Eine Möglichkeit ist der PasswordFlow.
-                options.AllowPasswordFlow()
-                       .AllowRefreshTokenFlow();
-
-                // Das erleichert die Entwicklung in Produktion aber ein no go
-                options.DisableHttpsRequirement();
-
-                // Dies aktiviert die Anwendung des JWT-Standards
-                options.UseJsonWebTokens();
-
-                // Für die Signierung der JWT-Tokens wird ein Zertifikat benötigt.
-                // Für DEV-Zwecke kann ein Key dynamisch erzeugt werden.
-                options.AddEphemeralSigningKey();
-            });
+            services.AddAuth(Configuration);
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
@@ -131,76 +33,9 @@ namespace AspNetCore.Security.OpenIddict
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
-            // Automatisches Mapping von JWT auf WS-Federation (Legacy) Tokens unterbinden
-            // Identity probiert Claims automatisch zu mappen. Durch das zurücksetzen
-            // der Mappingtabellen werden die Tokens nicht modifiziert
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-            JwtSecurityTokenHandler.DefaultOutboundClaimTypeMap.Clear();
-
-
-            // Die OAuth-Middleware muss möglichst früh in der Pipeline registriert werden.            
-            app.UseOAuthValidation();
-
-            // Die JWT-Middleware kann den Token aus dem Header extrahieren und das Request-Objekt damit erweitern
-            app.UseJwtBearerAuthentication(new JwtBearerOptions
-            {
-                Authority = "http://localhost:28476/",
-                Audience = "resource_server",
-                RequireHttpsMetadata = false,
-                TokenValidationParameters = new TokenValidationParameters
-                {
-                    NameClaimType = OpenIdConnectConstants.Claims.Subject,
-                    RoleClaimType = OpenIdConnectConstants.Claims.Role,
-                }
-            });
-            
-            app.UseOpenIddict();
+            app.UseAuth();
 
             app.UseMvc();
-
-
-            // Sicherstellen das ein paar User in der DB vorhanden sind.
-            InitializeAuthDb(app.ApplicationServices).Wait();
         }
-
-        private async Task InitializeAuthDb(IServiceProvider applicationServices)
-        {
-            using (var scope = applicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
-            {
-                var context = scope.ServiceProvider.GetRequiredService<IdentityContext>();
-                await context.Database.EnsureCreatedAsync();
-
-                var manager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-
-                await AddUser(manager, "chuck", "chuck@norris.com", "norris", GetManagerClaims());
-                await AddUser(manager, "jason", "jason@bourne.com", "bourne", GetSupportClaims());
-            }
-        }
-
-        private async Task AddUser(UserManager<ApplicationUser> manager, string username, string email, string password, IEnumerable<Claim> claims)
-        {
-            var user = await manager.FindByNameAsync(username);
-            if (user != null)
-                return;
-
-            var result = await manager.CreateAsync(new ApplicationUser { UserName = username, Email = email }, password);
-
-            user = await manager.FindByNameAsync(username);
-            await manager.AddClaimsAsync(user, claims);
-        }
-
-        private IEnumerable<Claim> GetManagerClaims()
-        {
-            yield return CreateClaim(ClaimTypes.PostalCode, "12345");
-            yield return CreateClaim(AppClaimTypes.Department, "Customer Management");
-        }
-
-        private IEnumerable<Claim> GetSupportClaims()
-        {
-            yield return CreateClaim(ClaimTypes.PostalCode, "54321");
-            yield return CreateClaim(AppClaimTypes.Department, "Customer Support");
-        }
-
-        private Claim CreateClaim(string type, string value) => new Claim(type, value, ClaimValueTypes.String, ClaimsIdentity.DefaultIssuer);
     }
 }
