63648afeeeb02c06ce7a9187918c524f9ef99e58
Florian Wachs <florian.wachs@live.de>
2017-06-01 13:56:40 +0200

    example working

---
+++ src/Exercises/AspNETCore.OpenIddict/AspNetCore.Security.OpenIddict/AspNetCore.Security.OpenIddict.Client/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCore.Security.OpenIddict.Client.Models
+{
+    public class Customer
+    {
+        public string Age { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/Exercises/AspNETCore.OpenIddict/AspNetCore.Security.OpenIddict/AspNetCore.Security.OpenIddict/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCore.Security.OpenIddict.Models
+{
+    public class Customer
+    {
+        public string Age { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
