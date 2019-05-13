8f517d9ec6f406389a6a848d55ded31703c5c2b5
Florian Wachs <florian.wachs@live.de>
2019-05-13 22:58:31 +0200

    first exercise

---
+++ 01_csharp/solutions/ConsoleChuckNorrisService/ChuckNorrisService/Client/ChuckNorrisApi.cs
@@ -0,0 +1,47 @@
+﻿using ChuckNorrisService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisService.Client
+{
+    public class ChuckNorrisApi
+    {
+        private const int MaxJokesPerRequest = 50;
+        private static readonly HttpClient _client = new HttpClient { BaseAddress = new Uri("https://api.chucknorris.io/jokes/random") };
+
+        public async Task<ChuckNorrisJoke> GetRandomJokeFromCategory(JokeCategories category)
+        {
+            var result = await _client.GetAsync($"?category={category.ToApiCategoryParameter()}");
+            result.EnsureSuccessStatusCode();
+
+            var joke = await result.Content.ReadAsAsync<ChuckNorrisJoke>();
+
+            return joke;
+        }
+
+        public async Task<ChuckNorrisJoke[]> GetRandomJokesFromCategory(JokeCategories category, int maxJokes)
+        {
+            maxJokes = Math.Clamp(maxJokes, 1, MaxJokesPerRequest);
+            var result = await Task.WhenAll(Enumerable.Range(0, maxJokes).Select(_ => GetRandomJokeFromCategory(category)));
+            var uniqueIds = new HashSet<string>();
+            return result.Where(joke => uniqueIds.Add(joke.Id)).ToArray();
+        }
+
+        public class ChuckNorrisJoke
+        {
+            public string[] Category { get; set; }
+
+            public Uri IconUrl { get; set; }
+
+            public string Id { get; set; }
+
+            public Uri Url { get; set; }
+
+            public string Value { get; set; }
+        }
+    }
+}
---
+++ 01_csharp/solutions/ConsoleChuckNorrisService/ChuckNorrisService/Program.cs
@@ -0,0 +1,58 @@
+﻿using ChuckNorrisService.Client;
+using ChuckNorrisService.Models;
+using ChuckNorrisService.Providers;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisService
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Aufgabe 1
+            await OutputRandomJokeFromApiAsync();
+
+            // Aufgabe 2
+            await SaveJokesToFileAsync();
+
+            // Aufgabe 3
+            await OutputRandomJokeFromFileSystemAsync();
+        }
+
+        private static async Task OutputRandomJokeFromApiAsync()
+        {
+            var api = new ChuckNorrisApi();
+            var randomJoke = await api.GetRandomJokeFromCategory(JokeCategories.Dev);
+            Console.WriteLine(randomJoke.Value);
+        }
+
+        private static async Task SaveJokesToFileAsync()
+        {
+            var api = new ChuckNorrisApi();
+            var result = await api.GetRandomJokesFromCategory(JokeCategories.Dev, 10);
+
+            SaveJokes(result);
+
+            void SaveJokes(ChuckNorrisApi.ChuckNorrisJoke[] jokesToSerialize)
+            {
+                // TODO: Nur für Vorlesungszwecke! Das referenzieren von Strings erzeugt
+                // eine Kopie, problematisch bei großen strings (LOH)
+                // Mit .net core 3.0 wird es performantere Möglichkeiten zur
+                // Serialisierung und Deserialisierung geben.
+
+                var raw = JsonConvert.SerializeObject(jokesToSerialize);
+                File.WriteAllText($"jokes_{DateTime.Now.Ticks}.json", raw);
+            }
+        }
+
+        private static async Task OutputRandomJokeFromFileSystemAsync()
+        {
+            var provider = new FileSystemJokesProvider();
+            var joke = await provider.GetRandomJokeAsync();
+            Console.WriteLine(joke.Value);
+        }
+    }
+}
