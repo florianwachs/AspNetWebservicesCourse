1ac27b8d1de6c4fe5fa41a035ac6d5186870ec70
Florian Wachs <2216547+florianwachs@users.noreply.github.com>
2019-06-02 14:18:02 +0200

    initial lesson checkin

---
+++ 04_aspnetcoreautomapper/lessons/AutomapperLesson/AutomapperLesson.Domain/AccountManagement/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomapperLesson.Domain.AccountManagement
+{
+    public class UserProfile
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public bool IsAdmin { get; set; }
+        public string CreditCardNumber { get; set; }
+    }
+}
---
+++ 04_aspnetcoreautomapper/lessons/AutomapperLesson/AutomapperLesson.Infrastructure/DataAccess/UserProfileRepository.cs
@@ -0,0 +1,29 @@
+﻿using AutomapperLesson.Domain.AccountManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomapperLesson.Infrastructure.DataAccess
+{
+    public class UserProfileRepository : IUserProfileRepository
+    {
+        private Dictionary<string, UserProfile> _profiles = new Dictionary<string, UserProfile>
+        {
+            ["1"] = new UserProfile { Id = "1", FirstName = "Chuck", LastName = "Norris", Age = 28, IsAdmin = true, CreditCardNumber = "12345" },
+            ["2"] = new UserProfile { Id = "2", FirstName = "Jason", LastName = "Bourne", Age = 74, IsAdmin = false, CreditCardNumber = "5674" },
+        };
+
+        public IReadOnlyCollection<UserProfile> All()
+        {
+            return _profiles.Values;
+        }
+
+        public UserProfile Update(UserProfile profile)
+        {
+            _profiles[profile.Id] = profile;
+            return profile;
+        }
+    }
+}
