60de46c4e1be686fb1afd2e124aaf115d6e64ac4
fexdev <florian.wachs@live.de>
2019-06-06 13:55:53 +0200

    solution

--- 05_aspnetcoreautomapper/solutions/automapper/AspNetCoreAutomapper/Api/Authors/AuthorsController.cs
+++ 05_aspnetcoreautomapper/solutions/automapper/AspNetCoreAutomapper/Api/Authors/AuthorsController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AspNetCoreAutomapper.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -5,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace AspNetCoreAutomapper.Api.Authors
 {
     [Route("api/[controller]")]
+    [ApiController]
     public class AuthorsController : ControllerBase
     {
         private readonly IAuthorRepository _authorRepository;
@@ -15,10 +17,15 @@ namespace AspNetCoreAutomapper.Api.Authors
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<ActionResult<IEnumerable<Author>>> GetAll()
         {
             return Ok(await _authorRepository.GetAll());
         }
-        
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Author>> UpdateAuthor(string id, [FromBody] Author author)
+        {
+            return Ok(await _authorRepository.Update(author));
+        }
     }
 }
