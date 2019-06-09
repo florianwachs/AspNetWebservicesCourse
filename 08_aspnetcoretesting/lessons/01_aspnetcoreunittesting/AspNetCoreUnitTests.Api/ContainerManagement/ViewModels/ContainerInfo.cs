7a99604705ba4b95e4edd1776700026b4f842eea
Florian Wachs <florian.wachs@live.de>
2019-06-09 16:48:18 +0200

    viewmodels

---
+++ 08_aspnetcoretesting/lessons/01_aspnetcoreunittesting/AspNetCoreUnitTests.Api/ContainerManagement/ViewModels/ContainerInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreUnitTests.Api.ContainerManagement.ViewModels
+{
+    public class ContainerInfo
+    {
+        public string Id { get; set; }
+        public decimal TotalWeightInKg { get; set; }
+    }
+}
