8b6c494e9238fabfc66d5b986a05ca38707c99b3
Florian Wachs <florian.wachs@live.de>
2019-06-09 18:41:30 +0200

    integration test config

--- 08_aspnetcoretesting/lessons/01_aspnetcoreunittesting/AspNetCoreUnitTests.IntegrationTests/ContainerManagement/ContainersControllerTests.cs
+++ 08_aspnetcoretesting/lessons/01_aspnetcoreunittesting/AspNetCoreUnitTests.IntegrationTests/ContainerManagement/ContainersControllerTests.cs
@@ -11,22 +11,26 @@ using Xunit;
 
 namespace AspNetCoreUnitTests.IntegrationTests.ContainerManagement
 {
-    public class ContainersControllerTests : IntegrationTestBase
+public class ContainersControllerTests : IntegrationTestBase
+{
+    public ContainersControllerTests(WebApplicationFactory<Api.Startup> factory) : base(factory)
     {
-        public ContainersControllerTests(WebApplicationFactory<Api.Startup> factory) : base(factory)
-        {
-        }
-
-        [Fact]
-        public async Task Should_be_able_to_get_all_container_infos()
-        {
-            var client = CreateClient();
-            var response = await client.GetAsync("/api/containers/infos");
-
-            Assert.True(response.IsSuccessStatusCode);
-            
-            var infos = await response.Content.ReadAsAsync<IEnumerable<ContainerInfo>>();
-            Assert.NotEmpty(infos);
-        }
     }
+
+    [Fact]
+    public async Task Should_be_able_to_get_all_container_infos()
+    {
+        // Ruft CreateClient() der Basisklasse auf
+        var client = CreateClient();
+
+        // Führt einen Request gegen die API In-Memory durch
+        var response = await client.GetAsync("/api/containers/infos");
+
+        // Wie beim echten HttpClient kann das Resultat überprüft und
+        // verarbeitet werden.
+        Assert.True(response.IsSuccessStatusCode);
+        var infos = await response.Content.ReadAsAsync<IEnumerable<ContainerInfo>>();
+        Assert.NotEmpty(infos);
+    }
+}
 }
--- 08_aspnetcoretesting/lessons/01_aspnetcoreunittesting/AspNetCoreUnitTests.IntegrationTests/IntegrationTestBase.cs
+++ 08_aspnetcoretesting/lessons/01_aspnetcoreunittesting/AspNetCoreUnitTests.IntegrationTests/IntegrationTestBase.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using AspNetCoreUnitTests.Api;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace AspNetCoreUnitTests.IntegrationTests
@@ -19,6 +21,28 @@ namespace AspNetCoreUnitTests.IntegrationTests
             _factory = factory;
         }
 
-        public HttpClient CreateClient() => _factory.CreateClient();
+        public HttpClient CreateClient() => _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                // Hier können Services aus der normalen Startup der App mit
+                // Fake / Mock-Services überschrieben werden
+                var serviceProvider = services.BuildServiceProvider();
+
+                using (var scope = serviceProvider.CreateScope())
+                {
+                    // Hiermit kann das DI-System hochgefahren werden
+                    // und z.B. eine In-Memory-DB mit Daten gefüllt werden.
+                    var scopedServices = scope.ServiceProvider;
+                    //var db = scopedServices
+                    //    .GetRequiredService<ApplicationDbContext>();
+                }
+            });
+
+            builder.ConfigureServices(services =>
+            {
+                // Hier können zusätzliche Services konfiguriert werden.
+            });
+        }).CreateClient();
     }
 }
