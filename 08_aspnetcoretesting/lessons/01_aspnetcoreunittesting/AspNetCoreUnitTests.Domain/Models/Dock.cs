43c54077aec892c181d61e8e3f2c80a7823134cb
Florian Wachs <florian.wachs@live.de>
2019-06-09 14:50:37 +0200

    init lesson done

---
+++ 08_aspnetcoretesting/lessons/01_aspnetcoreunittesting/AspNetCoreUnitTests.Domain/Models/Dock.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreUnitTests.Domain.Models
+{
+    public enum DockStatus
+    {
+        Unavailable,
+        Available,
+        Reserved,
+        Occupied,
+        Loading,
+        Unloading,
+    }
+
+    public class Dock
+    {
+        public string Id { get; private set; }
+        public string Identifier { get; private set; }
+        public string DockedShipId { get; private set; }
+        public DockStatus Status { get; private set; }
+        public bool IsOccupied => !string.IsNullOrWhiteSpace(DockedShipId);
+
+        public void DockShip(Ship ship)
+        {
+        }
+
+        public void UndockShip(Ship ship)
+        {
+        }
+
+        public void BeginLoading()
+        {
+        }
+
+        public void FinishedLoading()
+        {
+        }
+
+        public void BeginUnloading()
+        {
+        }
+
+        public void FinishedUnloading()
+        {
+        }
+
+    }
+}
---
+++ 08_aspnetcoretesting/lessons/01_aspnetcoreunittesting/AspNetCoreUnitTests.Domain/Models/Port.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreUnitTests.Domain.Models
+{
+    public class Port
+    {
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public IReadOnlyCollection<Dock> Docks { get; private set; }
+
+        private Port()
+        {
+            // For EF Core to hydrate entity from database
+            Docks = new List<Dock>();
+        }
+    }
+}
