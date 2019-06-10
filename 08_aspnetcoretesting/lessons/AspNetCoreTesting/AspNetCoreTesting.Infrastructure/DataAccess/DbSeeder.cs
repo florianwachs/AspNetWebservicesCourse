952bb2c75e6de89bd5a0c01e049cdb25ed0c55a7
Florian Wachs <florian.wachs@live.de>
2019-06-10 20:44:47 +0200

    cleanup

--- 08_aspnetcoretesting/lessons/AspNetCoreTesting/AspNetCoreTesting.Infrastructure/DataAccess/DbSeeder.cs
+++ 08_aspnetcoretesting/lessons/AspNetCoreTesting/AspNetCoreTesting.Infrastructure/DataAccess/DbSeeder.cs
@@ -1,52 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using AspNetCoreTesting.Domain.ApplicationServices;
 using AspNetCoreTesting.Domain.Domain;
-using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreTesting.Infrastructure.DataAccess
 {
     public static class DbSeeder
     {
+        private static readonly IdGenerator _id = new IdGenerator();
+
         public static void SeedDb(UniversityDbContext dbContext)
         {
             dbContext.Database.EnsureCreated();
-            var id = new IdGenerator();
 
-            if (!dbContext.Professors.Any())
-            {
-                dbContext.Professors.AddRange(new[]
-                {
-                    Professor.Create(id.NewEntityId(), "Jason", "Bourne", "jasonbourne@th-norris.de", "JaBo"),
-                    Professor.Create(id.NewEntityId(), "Chuck", "Norris", "chucknorris@th-norris.de", "ChNo"),
-                    Professor.Create(id.NewEntityId(), "Katie", "Bouman", "dr.katie.bouman@th-norris.de", "KaBo"),
-                });
+            SeedProfessors(dbContext);
+            SeedStudents(dbContext);
+            SeedCourses(dbContext);
+        }
 
-                dbContext.SaveChanges();
-            }
-
+        private static void SeedStudents(UniversityDbContext dbContext)
+        {
             if (!dbContext.Students.Any())
             {
                 dbContext.Students.AddRange(new[]
                 {
-                    Student.Create(id.NewEntityId(), "Hansi", "Hinterseer", "alpenkasperl@th-norris.de", "MaiHaHi"),
-                    Student.Create(id.NewEntityId(), "Florian", "Silberfischel", "silberfischel@th-norris.de", "FlSi"),
-                    Student.Create(id.NewEntityId(), "Seven", "Of Nine", "resistenceisfutile@th-norris.de", "SeNi"),
+                    Student.Create(_id.NewEntityId(), "Hansi", "Hinterseer", "alpenkasperl@th-norris.de", "MaiHaHi"),
+                    Student.Create(_id.NewEntityId(), "Florian", "Silberfischel", "silberfischel@th-norris.de", "FlSi"),
+                    Student.Create(_id.NewEntityId(), "Seven", "Of Nine", "resistenceisfutile@th-norris.de", "SeNi"),
                 });
 
                 dbContext.SaveChanges();
             }
+        }
 
+        private static void SeedCourses(UniversityDbContext dbContext)
+        {
             if (!dbContext.Courses.Any())
             {
                 dbContext.Courses.AddRange(new[]
                 {
-                    Course.Create(id.NewEntityId(), "Physics_1", "How to take a picture of a black hole", "How to take a picture of a black hole"),
-                    Course.Create(id.NewEntityId(), "SelfDefense_1", "Roundhouse Kick", "How to do the perfect Roundhouse Kick."),
-                    Course.Create(id.NewEntityId(), "Memory_1", "Memory lost, what now", "Guidelines to cope with memory loss."),
+                    Course.Create(_id.NewEntityId(), "Physics_1", "How to take a picture of a black hole", "How to take a picture of a black hole"),
+                    Course.Create(_id.NewEntityId(), "SelfDefense_1", "Roundhouse Kick", "How to do the perfect Roundhouse Kick."),
+                    Course.Create(_id.NewEntityId(), "Memory_1", "Memory lost, what now", "Guidelines to cope with memory loss."),
+                });
+
+                dbContext.SaveChanges();
+            }
+        }
+
+        private static void SeedProfessors(UniversityDbContext dbContext)
+        {
+            if (!dbContext.Professors.Any())
+            {
+                dbContext.Professors.AddRange(new[]
+                {
+                    Professor.Create(_id.NewEntityId(), "Jason", "Bourne", "jasonbourne@th-norris.de", "JaBo"),
+                    Professor.Create(_id.NewEntityId(), "Chuck", "Norris", "chucknorris@th-norris.de", "ChNo"),
+                    Professor.Create(_id.NewEntityId(), "Katie", "Bouman", "dr.katie.bouman@th-norris.de", "KaBo"),
                 });
 
                 dbContext.SaveChanges();
