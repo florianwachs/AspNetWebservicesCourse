16d99c794d5ece7a647efba0a77c1df58d3625ca
Florian Wachs <florian.wachs@live.de>
2019-06-10 20:41:03 +0200

    DI Registrations separated

---
+++ 08_aspnetcoretesting/lessons/AspNetCoreTesting/AspNetCoreTesting.Api/DIRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using FluentValidation.AspNetCore;
+using AutoMapper;
+using Microsoft.Data.Sqlite;
+using AspNetCoreTesting.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreTesting.Api
+{
+    public static class DIRegistration
+    {
+        public static void AddUniversityServices(this IServiceCollection services)
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            services.AddDbContext<UniversityDbContext>(options =>
+            {
+                options.UseSqlite(connection);
+            });
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        }
+    }
+}
--- 08_aspnetcoretesting/lessons/AspNetCoreTesting/AspNetCoreTesting.Api/Startup.cs
+++ 08_aspnetcoretesting/lessons/AspNetCoreTesting/AspNetCoreTesting.Api/Startup.cs
@@ -1,44 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AspNetCoreTesting.Infrastructure.DataAccess;
+﻿using AspNetCoreTesting.Infrastructure.DataAccess;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace AspNetCoreTesting.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            var connection = new SqliteConnection("DataSource=:memory:");
-            connection.Open();
-
-            services.AddDbContext<UniversityDbContext>(options =>
-            {
-                options.UseSqlite(connection);
-            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2).AddFluentValidation();
+            services.AddUniversityServices();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, UniversityDbContext universityDb)
         {
             if (env.IsDevelopment())
@@ -47,7 +28,6 @@ namespace AspNetCoreTesting.Api
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
