038c95e810ee69caaa1ddd2a14be1967a6c418fe
Florian Wachs <florian.wachs@live.de>
2019-06-20 19:31:19 +0200

    predocker

---
+++ 13_aspnetcoremicroservices/lessons/AspNetCoreMicroservices/AspNetCoreMicroservices.Frontend/ApiGateway/JokesController.cs
@@ -0,0 +1,34 @@
+﻿using AspNetCoreMicroservices.Frontend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMicroservices.Frontend.ApiGateway
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JokesController : ControllerBase
+    {
+        // Naiver Ansatz, besser: HttpClientFactory
+        private static readonly HttpClient _client = new HttpClient();
+        private readonly IOptionsSnapshot<ApiConfig> _config;
+        private readonly Uri _baseUri;
+
+        public JokesController(IOptionsSnapshot<ApiConfig> config)
+        {
+            _config = config;
+            _baseUri = new Uri(config.Value.JokesServiceBaseUri);
+        }
+
+        public async Task<ActionResult<IEnumerable<JokeDto>>> GetBooks()
+        {
+            HttpResponseMessage response = await _client.GetAsync(new Uri(_baseUri, "api/jokes"));
+            response.EnsureSuccessStatusCode();
+
+            return Ok(await response.Content.ReadAsAsync<IEnumerable<JokeDto>>());
+        }
+    }
+}
---
+++ 13_aspnetcoremicroservices/lessons/AspNetCoreMicroservices/AspNetCoreMicroservices.Frontend/Models/JokeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMicroservices.Frontend.Models
+{
+    public class JokeDto
+    {
+        public string Id { get; set; }
+        public string JokeText { get; set; }
+    }
+}
