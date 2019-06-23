71c25517595e4015c72db986a361879c9f56fbe5
fexdev <florian.wachs@live.de>
2019-06-23 20:46:10 +0200

    seed samples

--- 04_aspnetcoreef/lessons/EfCoreRelationSample/EfCoreRelationSample/DataAccess/DbSeeder.cs
+++ 04_aspnetcoreef/lessons/EfCoreRelationSample/EfCoreRelationSample/DataAccess/DbSeeder.cs
@@ -20,12 +20,26 @@ namespace EfCoreRelationSample.DataAccess
                 return;
             }
 
-            SeedLists(dbContext);
+            var list1 = List.Create(_id.NewEntityId(), "Shared With All");
+            var list2 = List.Create(_id.NewEntityId(), "Black Hole Pictures");
+
+            var user1 = User.Create(_id.NewEntityId(), "Katie Bouman");
+            var user2 = User.Create(_id.NewEntityId(), "Chuck Norris");
+            var user3 = User.Create(_id.NewEntityId(), "Jean Claude van Damme");
+
+            list1.ShareWithUser(user1);
+            list1.ShareWithUser(user2);
+            list1.ShareWithUser(user3);
+
+            list2.ShareWithUser(user1);
+
+            dbContext.Lists.AddRange(new[] { list1, list2 });
+            dbContext.Users.AddRange(new[] { user1, user2, user3 });
+
+            dbContext.SaveChanges();
+
         }
 
-        private static void SeedLists(DemoDbContext dbContext)
-        {
 
-        }
     }
 }
