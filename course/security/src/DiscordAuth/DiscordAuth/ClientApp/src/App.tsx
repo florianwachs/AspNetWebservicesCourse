8ccec318f6b5ee277675cf4271ab271ca5a3d261
Florian Wachs <2216547+florianwachs@users.noreply.github.com>
2020-07-15 13:31:46 +0200

    Discord Sample

---
+++ course/security/src/DiscordAuth/DiscordAuth/ClientApp/src/App.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as api from "./services/apiClient";
+
+function App() {
+  let [isLoggedIn, setIsLoggedIn] = useState(false);
+  let [userInfo, setUserInfo] = useState<api.IUserInfo>();
+
+  useEffect(() => {
+    let checkLogin = async () => {
+      setIsLoggedIn(await api.isLoggedIn());
+    };
+    checkLogin();
+  }, []);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      return;
+    }
+
+    let getUserInfo = async () => {
+      let result = await api.getUserInfo();
+      setUserInfo(result);
+    };
+    getUserInfo();
+  }, [isLoggedIn]);
+
+  return <div className="App">{isLoggedIn ? <UserInfoDisplay info={userInfo} /> : <a href="account/discord/login">Login with Discord</a>}</div>;
+}
+
+export default App;
+
+const UserInfoDisplay: React.FC<{ info: api.IUserInfo | undefined }> = ({ info }) => {
+  if (!info) {
+    return <h1>Loading</h1>;
+  }
+
+  return (
+    <ul>
+      <li>Username: {info.username}</li>
+    </ul>
+  );
+};
---
+++ course/security/src/DiscordAuth/DiscordAuth/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordAuth.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet("token")]
+        [Authorize]
+        public async Task<IActionResult> GetToken()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var authenticationInfo = await HttpContext.AuthenticateAsync();
+            return Ok(accessToken);
+        }
+
+
+        [HttpGet("discord/login")]
+        public IActionResult Login(string redirectUri = "/")
+        {
+            var authenticationProperties = new AuthenticationProperties
+            {
+                RedirectUri = redirectUri,
+            };
+
+            return Challenge(authenticationProperties, "Discord");
+        }
+
+
+    }
+}
