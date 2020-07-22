e07f47f24d801ac13a7e610e9fbbedc71a6211fd
Florian Wachs <Florian.Wachs@halvotec.de>
2020-07-22 16:48:33 +0200

    grpc server

---
+++ course/grpc/src/AspNetCoreGrpc/AspNetCoreGrpc.Api/Services/SensorService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreGrpc.Api.Services
+{
+    public class SensorService: SensorReadingService.SensorReadingServiceBase
+    {
+        public override Task<SensorResponseMessage> AddReading(SensorReadingPackage request, ServerCallContext context)
+        {
+            // Do Stuff
+            var result = new SensorResponseMessage { Success = true, Message = "Got your reading" };
+
+            foreach (var sensorData in request.Readings)
+            {
+                // Save all readings
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+}
