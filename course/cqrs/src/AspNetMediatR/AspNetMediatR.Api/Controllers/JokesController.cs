68b2c8b5c6655c09eaf45b6f6f045f7e11d2d856
Florian Wachs <Florian.Wachs@halvotec.de>
2020-07-23 14:38:38 +0200

    feat(cqrs): mediatr sample

---
+++ course/cqrs/src/AspNetMediatR/AspNetMediatR.Api/Controllers/JokesController.cs
@@ -0,0 +1,26 @@
+﻿using AspNetMediatR.Api.Domain.Jokes.Models;
+using AspNetMediatR.Api.Domain.Jokes.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AspNetMediatR.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JokesController : ControllerBase
+    {
+
+        public JokesController(IMediator mediator)
+        {
+            Mediator = mediator;
+        }
+
+        public IMediator Mediator { get; }
+
+        [HttpGet]
+        public async Task<IActionResult> GetJokes() => Ok(await Mediator.Send(new JokesQuery(), CancellationToken.None));
+    }
+}
---
+++ course/cqrs/src/AspNetMediatR/AspNetMediatR.Api/Startup.cs
@@ -0,0 +1,51 @@
+using AspNetMediatR.Api.Domain.Jokes.Models;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AspNetMediatR.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                    .AddFluentValidation();
+
+            services.AddJokesServices();
+            services.AddMediatR(typeof(Joke).Assembly);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
