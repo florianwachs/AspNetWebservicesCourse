3446f46e33e6d9d5eb16c8c98d5496f88369d6f9
Florian Wachs <Florian.Wachs@halvotec.de>
2021-05-26 11:02:19 +0200

    feat: .net 6 Minimal API sample

---
+++ 03_aspnetcorebasics/lessons/MinApi/Models/Joke.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace ChuckNorrisService.Models
+{
+    public class Joke
+    {
+        public string Id { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string JokeText { get; set; }
+        public IReadOnlyCollection<JokeCategory> Categories { get; set; }
+
+
+    }
+
+    public class JokeValidator : AbstractValidator<Joke>
+    {
+        public JokeValidator()
+        {
+            RuleFor(j => j.JokeText).NotEmpty().MaximumLength(500);
+            When(j => j.Categories != null && j.Categories.Any(), () =>
+              {
+                  RuleFor(j => j.Categories).Must(BeUnique).WithMessage("Categories must be unique");
+              });
+        }
+
+        private bool BeUnique(IReadOnlyCollection<JokeCategory> categories)
+        {
+            return categories.Count == categories.Distinct().Count();
+        }
+    }
+
+    public class JokeDto
+    {
+        public string Id { get; set; }
+        public string JokeText { get; set; }
+        public string[] Category { get; set; }
+    }
+}
---
+++ 03_aspnetcorebasics/lessons/MinApi/OldController/JokesController.cs
@@ -0,0 +1,44 @@
+﻿using ChuckNorrisService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JokesController : ControllerBase
+    {
+        private readonly IJokeRepository _jokeProvider;
+
+        public JokesController(IJokeRepository jokeProvider)
+        {
+            _jokeProvider = jokeProvider;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            Joke joke = await _jokeProvider.GetById(id);
+            if (joke == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(joke);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _jokeProvider.Delete(id);
+            return Ok();
+        }
+
+        [HttpGet("random")]
+        public async Task<IActionResult> GetRandomJoke()
+        {
+            return Ok(await _jokeProvider.GetRandomJoke());
+        }
+    }
+}
