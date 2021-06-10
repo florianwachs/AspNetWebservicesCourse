85b5284c22e832624e390b4e76b9d038be64af6c
Florian Wachs <Florian.Wachs@halvotec.de>
2021-06-10 09:30:57 +0200

    feat(akka):sample

---
+++ 14_actors/src/AkkaDocumentGenerator/Actors/ReportGenerationControllerActor.cs
@@ -0,0 +1,101 @@
+﻿using Akka.Actor;
+using Akka.DI.Core;
+using Akka.Event;
+using Akka.Logger.Serilog;
+using Akka.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkkaDocumentGenerator.Actors
+{
+    public class ReportGenerationControllerActor : ReceiveActor
+    {
+        private ILoggingAdapter Log { get; }
+        private IActorRef ReportGeneratorActorRef { get; }
+
+        private Dictionary<Guid, ReportGeneratorActor.GenerateReportResult> Results { get; } = new();
+        private Dictionary<Guid, ReportStatusResult> Status { get; } = new();
+
+        public ReportGenerationControllerActor()
+        {
+            Log = Context.GetLogger<SerilogLoggingAdapter>();
+            Log.Info($"Creating {nameof(ReportGenerationControllerActor)}");
+
+            var reportGeneratorActorProps = ReportGeneratorActor.CreateProps(Context.System).WithRouter(new RoundRobinPool(10, new DefaultResizer(10, 30)));
+            ReportGeneratorActorRef = Context.ActorOf(reportGeneratorActorProps, "ReportGenerator");
+
+            Receive<GenerateReport>(HandleGenerateReport);
+            Receive<ReportStatus>(HandleReportStatus);
+            Receive<ReportGeneratorActor.GenerateReportSuccess>(HandleGenerateReportSuccess);
+            Receive<ReportGeneratorActor.GenerateReportFailed>(HandleGenerateReportFailed);
+        }
+
+        public static Props CreateProps(ActorSystem actorSystem) => actorSystem.DI().Props<ReportGenerationControllerActor>();
+
+        #region Handlers
+        private void HandleGenerateReportFailed(ReportGeneratorActor.GenerateReportFailed request)
+        {
+            Results[request.GenerationId] = request;
+            var status = new ReportStatusResult(request.GenerationId, ReportGenerationStatus.Failed);
+            Log.Error("Failed to generate {@Report}", status);
+            Status[request.GenerationId] = status;
+        }
+
+        private void HandleGenerateReportSuccess(ReportGeneratorActor.GenerateReportSuccess request)
+        {
+            Results[request.GenerationId] = request;
+            var status = new ReportStatusResult(request.GenerationId, ReportGenerationStatus.Completed);
+            Log.Info("Success to generate {@Report}", status);
+            Status[request.GenerationId] = status;
+        }
+
+        private void HandleGenerateReport(GenerateReport request)
+        {
+            var status = new ReportStatusResult(Guid.NewGuid(), ReportGenerationStatus.InQueue);
+            Status[status.GenerationId] = status;
+            var generateReportRequest = new ReportGeneratorActor.GenerateReport(status.GenerationId, request.ReportName, request.ReportData);
+
+            Log.Info("Created new Report Generation Request {GenerationId} for {Report}", generateReportRequest.GenerationId, generateReportRequest.ReportName);
+            ReportGeneratorActorRef.Tell(generateReportRequest);
+            Sender.Tell(status);
+        }
+
+        private void HandleReportStatus(ReportStatus request)
+        {
+            ReportStatusResult status = default;
+            if (!Status.TryGetValue(request.GenerationId, out status))
+            {
+                status = new ReportStatusResult(request.GenerationId, ReportGenerationStatus.Unknown);
+            }
+
+            Sender.Tell(status);
+        }
+
+        protected override void Unhandled(object message)
+        {
+            Log.Warning("Message {Message} was unhandled", message);
+        }
+
+        #endregion
+
+
+        #region Messages
+
+        public record GenerateReport(string ReportName, object ReportData);
+        public record ReportStatus(Guid GenerationId);
+        public record ReportStatusResult(Guid GenerationId, ReportGenerationStatus Status);
+
+        public enum ReportGenerationStatus
+        {
+            Unknown,
+            InQueue,
+            Completed,
+            Failed,
+        }
+
+        #endregion
+    }
+}
---
+++ 14_actors/src/AkkaDocumentGenerator/Actors/ReportGeneratorActor.cs
@@ -0,0 +1,61 @@
+﻿using Akka.Actor;
+using Akka.DI.Core;
+using Akka.Event;
+using Akka.Logger.Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkkaDocumentGenerator.Actors
+{
+    public class ReportGeneratorActor : ReceiveActor
+    {
+        private static Random rnd = new Random();
+        private ILoggingAdapter Log { get; }
+        public ReportGeneratorActor()
+        {
+            Log = Context.GetLogger<SerilogLoggingAdapter>();
+            Log.Info($"Creating {nameof(ReportGeneratorActor)}");
+
+            ReceiveAsync<GenerateReport>(HandleGenerateReport);
+        }
+
+        public static Props CreateProps(ActorSystem actorSystem) => actorSystem.DI().Props<ReportGeneratorActor>();
+
+        private async Task HandleGenerateReport(GenerateReport request)
+        {
+            // TODO:
+            // - ReportViewModel generieren
+            // - Report mit Telerik Reporting generieren
+            // - Report in Temp-Verzeichnis ablegen
+            // - Success / Failure an Sender zurück geben
+
+
+            Log.Info("Beginning Report Generation for {GenerationId} {Report}", request.GenerationId, request.ReportName);
+            await Task.Delay(TimeSpan.FromSeconds(rnd.Next(10, 20)));
+
+            if (rnd.Next(0, 11) > 5)
+            {
+                Log.Info("Completed Report Generation for {GenerationId} {Report}", request.GenerationId, request.ReportName);
+                Sender.Tell(new GenerateReportSuccess(request.GenerationId, $"[PathToReport]-{request.GenerationId}"));
+            }
+            else
+            {
+                Log.Error("Failed Report Generation for {GenerationId} {Report}", request.GenerationId, request.ReportName);
+                Sender.Tell(new GenerateReportFailed(request.GenerationId, $"[FailReason]-{request.GenerationId}"));
+            }
+        }
+
+        #region Messages
+
+        public record GenerateReport(Guid GenerationId, string ReportName, object ReportData);
+
+        public record GenerateReportResult;
+        public record GenerateReportSuccess(Guid GenerationId, string GeneratedReportPath) : GenerateReportResult;
+        public record GenerateReportFailed(Guid GenerationId, string FailReason) : GenerateReportResult;
+
+        #endregion
+    }
+}
