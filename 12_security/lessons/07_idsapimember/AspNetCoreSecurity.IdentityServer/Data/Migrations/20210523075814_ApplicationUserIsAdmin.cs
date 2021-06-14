b8920b79e69639eb359cc5562a6ed0b0c057a256
Florian Wachs <Florian.Wachs@halvotec.de>
2021-06-14 08:09:40 +0200

    feat: security sample for students init

---
+++ 12_security/lessons/07_idsapimember/AspNetCoreSecurity.IdentityServer/Data/Migrations/20210523075814_ApplicationUserIsAdmin.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StsServerIdentity.Data.Migrations
+{
+    public partial class ApplicationUserIsAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "AspNetUsers");
+        }
+    }
+}
