7420769f3ae77f889353a4e68671ee611e70f025
Florian Wachs <2216547+florianwachs@users.noreply.github.com>
2022-05-12 10:01:25 +0200

    ef exercicse 1

---
+++ modules/aspnet_ef/exercises/readme.md
@@ -0,0 +1,34 @@
+# Übung AspNetCoreChuckNorrisService Entity Framework Core
+
+In den vorangegangenen Übungen haben wir immer mit Daten In-Memory gearbeitet. Für viele Anwendungsfälle ist dies auch das optimale Vorgehen, für ebenso viele allerdings nicht. In der Vorlesung haben wir Entity Framework Core behandelt, Microsofts modernen Ansatz für die Datenhaltung in relationalen Datenbanken. In dieser Übung werden wir das Vorgestellte praktisch anwenden.
+
+Viel Spaß dabei! :tada:
+
+Verbesserungsvorschläge / Ideen sind immer willkommen!
+
+> Nutzen Sie auch [Microsoft Docs](https://docs.microsoft.com).
+
+## Aufgabe 1
+
+Vorgegeben ist eine Solution welche bereits das Grundgerüst aus der vorangegangenen CRUD Übung enthält. Stellen Sie mit einem HttpClient Ihrer Wahl sicher, das die API auf Ihrem System funktioniert.
+
+## Aufgabe 2
+
+- [ ] Ergänzen Sie `Joke` und `JokeCategory` um notwendige Attribute, wir möchten nicht das die Ids von der Datenbank generiert werden
+- [ ] Implementieren Sie einen `DbJokeContext` für die Modelklassen `Joke` und `JokeCategories`
+- [ ] Registrieren Sie den Context im DI-System und verwenden Sie die In-Memory-Database des EF Core Frameworks
+- [ ] Sorgen Sie dafür, dass Ihre Datenbank beim Starten mit Witzen und Kategorien gefüllt werdem
+
+## Aufgabe 3
+
+- [ ] Implementieren Sie eine Klasse `EFJokeRepository` welche das Interface `IJokeRepository` implementiert und Ihren `JokeDbContext` verwendet.
+- [ ] Registrieren Sie dieses `EFJokeRepository` in Ihrem DI-System
+- [ ] Testen Sie die API mit einem HTTP-Client Ihrer Wahl
+
+## Aufgabe 4
+
+- [ ] Nutzen Sie statt der In-Memory-Datenbank den SQL-Provider mit der LocalDb (solltet ihr System keine LocalDb haben, nutzen Sie bitte SQLite)
+- [ ] Erstellen Sie eine initiale Migration mit dem dotnet ef Tool
+- [ ] Führen Sie die Migration gegen die Datenbank aus
+
+> Hinweise: Googlen Sie den Connection-String für die LocalDb (oder Ihre Datenbank)
---
+++ modules/aspnet_ef/exercises/src/ChuckNorrisService/Providers/FileSystemJokeProvider.cs
@@ -0,0 +1,97 @@
+﻿using ChuckNorrisService.Dtos;
+using ChuckNorrisService.Models;
+using System.Text.Json;
+
+namespace ChuckNorrisService.Providers;
+
+public class FileSystemJokeProvider : IJokeProvider
+{
+    private static readonly Random random = new Random();
+    private static readonly string JokeFilePath = Path.Combine("Data", "jokes.json");
+    private List<Joke> _jokes = new();
+
+    public FileSystemJokeProvider()
+    {
+        Init();
+    }
+
+    public async Task<Joke> Add(Joke joke)
+    {
+        if (string.IsNullOrWhiteSpace(joke.Id))
+        {
+            joke.Id = Guid.NewGuid().ToString();
+        }
+
+        _jokes.Add(joke);
+        return joke;
+    }
+
+    public async Task<Joke> Update(Joke joke)
+    {
+        var existing = _jokes.FirstOrDefault(x => x.Id == joke.Id);
+        if (existing == null)
+        {
+            throw new InvalidOperationException($"No Joke found with id {joke.Id}");
+        }
+
+        _jokes.Remove(existing);
+        _jokes.Add(joke);
+        return joke;
+    }
+
+    public async Task Delete(string id)
+    {
+        _jokes.RemoveAll(x => x.Id == id);
+    }
+
+    public async Task<Joke?> GetJokeById(string id)
+    {
+        return _jokes.FirstOrDefault(j => j.Id == id);
+    }
+
+    public async Task<Joke> GetRandomJokeAsync()
+    {
+        return _jokes[random.Next(0, _jokes.Count + 1)];
+    }
+
+    public async Task<Joke[]> GetAll()
+    {
+        return _jokes.ToArray();
+    }
+
+    private void Init()
+    {
+        if (!File.Exists(JokeFilePath))
+        {
+            throw new InvalidOperationException($"no jokes file located in {JokeFilePath}");
+        }
+
+        var rawJson = File.ReadAllText(JokeFilePath);
+        var jokeDtos = JsonSerializer.Deserialize<List<JokeDto>>(rawJson) ?? new();
+
+        var allCategories = jokeDtos
+            .SelectMany(j => j.Category)
+            .Distinct()
+            .Where(cat => !string.IsNullOrWhiteSpace(cat))
+            .Select(cat => new JokeCategory() { Id = cat, Name = cat })
+            .ToDictionary(k => k.Id);
+
+        var result = new List<Joke>();
+
+        foreach (var jokeDto in jokeDtos)
+        {
+            var matchingCategories = jokeDto.Category?.Select(cat => allCategories[cat]).ToList();
+            result.Add(
+                new Joke
+                {
+                    Id = jokeDto.Id,
+                    Value = jokeDto.Value,
+                    Categories = matchingCategories ?? new()
+                });
+        }
+
+
+        _jokes = result;
+
+    }
+}
