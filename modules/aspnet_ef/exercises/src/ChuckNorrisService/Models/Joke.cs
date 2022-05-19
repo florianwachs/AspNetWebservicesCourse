4a12e65e68849af59ac382aa91c5660fb5875c2a
Florian Wachs <2216547+florianwachs@users.noreply.github.com>
2022-05-19 14:04:25 +0200

    ef sample

--- modules/aspnet_ef/exercises/src/ChuckNorrisService/Models/Joke.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChuckNorrisService.Models
-{
-    public class Joke
-    {
-        public string Id { get; set; } = "";
-        public string Value { get; set; }= "";
-        public List<JokeCategory> Categories { get; set; } = new();
-    }
-}
