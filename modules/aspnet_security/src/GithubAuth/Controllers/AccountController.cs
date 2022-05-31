7d128d42d2ffec7713b414848271bed413496d63
Florian Wachs <Florian.Wachs@halvotec.de>
2022-05-31 17:15:46 +0200

    github auth sample

---
+++ modules/aspnet_security/src/GithubAuth/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using AspNet.Security.OAuth.GitHub;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GithubAuth.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController : ControllerBase
+{
+    [HttpGet("token")]
+    [Authorize]
+    public async Task<IActionResult> GetToken()
+    {
+        var accessToken = await HttpContext.GetTokenAsync("access_token");
+        var authenticationInfo = await HttpContext.AuthenticateAsync();
+        return Ok(accessToken);
+    }
+
+    [HttpGet("github/login")]
+    public IActionResult Login(string? redirectUri = "/")
+    {
+        var authenticationProperties = new AuthenticationProperties
+        {
+            RedirectUri = redirectUri,
+        };
+
+        return Challenge(authenticationProperties, GitHubAuthenticationDefaults.AuthenticationScheme);
+    }
+}
---
+++ modules/aspnet_security/src/GithubAuth/Infrastructure/GithubApiClient.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Headers;
+
+namespace GithubAuth.Infrastructure;
+
+public class GithubApiClient
+{
+    public GithubApiClient(HttpClient client)
+    {
+        client.BaseAddress = new Uri("https://api.github.com/");
+        client.DefaultRequestHeaders.Add("Accept", "application/json");
+        Client = client;
+    }
+
+    public HttpClient Client { get; }
+
+    public async Task<UserInfo> GetUserInfo(string token)
+    {
+        var userInfoResponse = await Client.SendAsync(GetWithToken(token, "user"));
+        userInfoResponse.EnsureSuccessStatusCode();
+        var result = await userInfoResponse.Content.ReadFromJsonAsync<UserInfo>();
+
+        return result;
+    }
+
+    private static HttpRequestMessage GetWithToken(string token, string urlPart)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, urlPart);
+        request.Headers.Authorization = new AuthenticationHeaderValue("token", token);
+        request.Headers.UserAgent.Clear();
+        request.Headers.UserAgent.Add(new ProductInfoHeaderValue("GithubTest", "v1"));
+        return request;
+    }
+
+    public record UserInfo(string Login, long Id, string Avatar_url, string Name, string Company);
+}
